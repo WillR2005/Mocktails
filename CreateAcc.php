c0acec5dba8d870d26073a8f6ed7d8ea9861b494
WillR2005 <168192440+WillR2005@users.noreply.github.com>
2024-05-23 13:16:33 +0100

    o

--- CreateAcc.php
+++ CreateAcc.php
@@ -5,6 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NewAccount</title>
 </head>
+<style>
+        body{background-color: #DAF2F2;
+        }
+        header{
+          background-color:#F5AFA7 ;
+        height:2vw ;
+        border: thin outset #e7d5d3 ;
+        display: flex;
+      position: fixed;
+     top: 0; 
+     left: 0; 
+     right: 0;
+      }
+      footer{
+        background-color:#A46097;
+        padding: 200px;
+        display: flex;
+        position: fixed;
+        
+        
+       left: 0; 
+       right: 0;
+      }
+      main{
+        height:550px ;
+        width: 500px;
+        max-width: 50vw;
+        background-color: rgb(230, 203, 230);
+        border: 3px solid #C6A6C7 ;
+        
+        padding: 5px;
+        display:grid;
+        grid: auto / auto auto auto;
+        margin-left: auto;
+        margin-right: auto;
+       
+        
+        
+      }
+</style>
+<header></header>
+
 <body>
 <main>
 <form method="post" action="addAcc.php">
@@ -22,6 +64,7 @@
 </form>
 
     </main>  
-   
+
+   <footer> </footer>
 </body>
 </html>
